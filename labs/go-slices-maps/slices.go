8a4462382b5a58bc89df4a3c3315e2b6d0814ef6
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-01-27 18:16:54 -0600

    FEATURE: Slices excersice done

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,17 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+//Pic .
+func Pic(dx, dy int) [][]uint8 {
+	picture := make([][]uint8, dy)
+	for i := range picture {
+		picture[i] = make([]uint8, dx)
+	}
+
+	return picture
+}
+
 func main() {
+	pic.Show(Pic)
 }
